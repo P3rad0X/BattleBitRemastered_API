123606613fb916ab216158944d2ba4eb39f28b83
Pieter Wentzel <pietercarelwentzel@gmail.com>
2023-08-29 19:54:05 +0200

    Added a class DiscordWebHookClient to push stuff to discord

---
+++ DiscordWebhookClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DiscordWebhook
+{ 
+    public class DiscordWebhookClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _webhookUrl;
+
+        public DiscordWebhookClient(string webhookUrl)
+        {
+            _httpClient = new HttpClient();
+            _webhookUrl = webhookUrl;
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            using StringContent jsonContent = new StringContent(
+                JsonSerializer.Serialize(new { content = message }),
+                Encoding.UTF8,
+                "application/json");
+
+            await _httpClient.PostAsync(_webhookUrl, jsonContent);
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -5,44 +5,77 @@ using System.Threading.Channels;
 using System.Xml;
 using System;
 using System.IO;//Will be using sytem and system.IO for saving chats to text file.
+using System.Text;
+using System.Text.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using DiscordWebhook;
+
 
 
 internal class Program
-
 {
 
+    public const string DiscordWebhookUrl = "https://discord.com/api/webhooks/1146078070378348544/yYc9L0mjDbzuWpPJUzTKekouVTdsBd7x7gKNk6Zxtu8HWo13dbHy45ZDIZZvj1RRLFkJ";
+    private static DiscordWebhookClient webhookClient;
 
+    
     private static void Main(string[] args)
     {
+        webhookClient = new DiscordWebhookClient(DiscordWebhookUrl);
+
         var listener = new ServerListener<MyPlayer, MyGameServer>();
         listener.LogLevel = LogLevel.Sockets | LogLevel.All;
         listener.OnLog += OnLog;
         listener.Start(29294);//Start Listener
         Console.WriteLine("Started Listener on port: 29294");
         Thread.Sleep(-1);
+      
     }
-
     private static void OnLog(LogLevel level, string msg, object? obj)
     {
         Console.WriteLine("Log (" + level + "): " + msg);    
+    }
+
+    public static void SendWebhookMessage(string message)
+    {
+        webhookClient.SendMessageAsync(message).Wait();
     }
 }
 
 internal class MyPlayer : Player<MyPlayer>
 {
-
+    
 
 
 }
 
 internal class MyGameServer : GameServer<MyPlayer>
 {
+   
+    public override async Task OnPlayerConnected(MyPlayer player)
+    {
+      
+        ForceStartGame();
+       
+    }
 
+    public override async Task OnRoundStarted()
+    {
+        string gameMap = Map;
+        int playerCount = CurrentPlayerCount;
+        string message = $"{playerCount} players";
+        Program.SendWebhookMessage(message);
+       
 
+    }
 
-
-
-
+    public override Task<bool> OnPlayerTypedMessage(MyPlayer player, ChatChannel channel, string msg)
+    {
+        ForceEndGame();
+        return base.OnPlayerTypedMessage(player, channel, msg);
+    }
 
 
 
